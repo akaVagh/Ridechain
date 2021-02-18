4fa88eeb4849fbf17dda98284056c60435271f77
akaVagh <harshvaghani00@gmail.com>
2021-02-18 18:33:43 +0530

    misc.

--- src/Screens/SplashScreen/index.jsx
+++ src/Screens/SplashScreen/index.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StatusBar, TouchableOpacity } from 'react-native';
 import styles from './styles';
 import Logo from '../../Components/Logo';
 import { Ionicons } from '@expo/vector-icons';
@@ -7,29 +7,30 @@ import * as Animatable from 'react-native-animatable';
 import { useNavigation } from '@react-navigation/native';
 
 const SplashScreen = (props) => {
-    const navigation = useNavigation();
+	const navigation = useNavigation();
 
-    return (
-        <View style={styles.container}>
-            <Animatable.View style={styles.header} animation='bounceIn'>
-                <Logo />
-            </Animatable.View>
-            <Animatable.View style={styles.footer} animation='fadeInUpBig'>
-                <Text style={styles.title}>
-                    Thank you for choosing RideChain!
-                </Text>
-                <Text style={styles.text}>Sign in with account</Text>
-                <TouchableOpacity
-                    onPress={() => navigation.navigate('SignIn')}
-                    style={styles.button}
-                >
-                    <View style={styles.signIn}>
-                        <Text style={styles.textSign}>Sign In</Text>
-                        <Ionicons name='log-in' size={25} color='white' />
-                    </View>
-                </TouchableOpacity>
-            </Animatable.View>
-        </View>
-    );
+	return (
+		<View style={styles.container}>
+			<StatusBar backgroundColor='#000' barStyle='light-content' />
+			<Animatable.View style={styles.header} animation='bounceIn'>
+				<Logo />
+			</Animatable.View>
+			<Animatable.View style={styles.footer} animation='fadeInUpBig'>
+				<Text style={styles.title}>
+					Thank you for choosing RideChain!
+				</Text>
+				<Text style={styles.text}>Sign in with account</Text>
+				<TouchableOpacity
+					onPress={() => navigation.navigate('SignIn')}
+					style={styles.button}
+				>
+					<View style={styles.signIn}>
+						<Text style={styles.textSign}>Sign In</Text>
+						<Ionicons name='log-in' size={25} color='white' />
+					</View>
+				</TouchableOpacity>
+			</Animatable.View>
+		</View>
+	);
 };
 export default SplashScreen;
